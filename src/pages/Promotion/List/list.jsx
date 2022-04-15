edf8622b83a06f7a62c08697c3c8049a21145d86
FRANCISCO WILLIAM DE OLIVEIRA LIMA <francisco.wolima@senacsp.edu.br>
2022-04-14 21:29:01 -0300

    chamando api fake

--- src/pages/Promotion/List/list.jsx
+++ src/pages/Promotion/List/list.jsx
@@ -1,28 +1,45 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import PromotionCard from '../../../componets/Promotion/card/Card'
+import axios from 'axios';
 
 const PagesPromotionlist = () => {
 
-    const promotion = {
-        id: 1,
-        title:
-          "Kit Notebook Acer Aspire 3 + Mochila Green, A315-41-R790, AMD Ryzen 3 2200U Dual Core",
-        price: 1799,
-        imageUrl:"https://m.media-amazon.com/images/I/61o0lt6Sh6L._AC_SX679_.jpg",
-         URL: "https://www.amazon.com.br/Notebook-Acer-Mochila-A315-41-R790-Memória/dp/B07YDWLV7S",
-        comments: [
-          {
-            id: 1,
-            comment: "TELA HD",
-            promotionId: 1,
-          },
-        ]
+    // const promotion = {
+    //     id: 1,
+    //     title:
+    //       "Kit Notebook Acer Aspire 3 + Mochila Green, A315-41-R790, AMD Ryzen 3 2200U Dual Core",
+    //     price: 1799,
+    //     imageUrl:"https://m.media-amazon.com/images/I/61o0lt6Sh6L._AC_SX679_.jpg",
+    //      URL: "https://www.amazon.com.br/Notebook-Acer-Mochila-A315-41-R790-Memória/dp/B07YDWLV7S",
+    //     comments: [
+    //       {
+    //         id: 1,
+    //         comment: "TELA HD",
+    //         promotionId: 1,
+    //       },
+    //     ]
+    //   }
+    const [promotions, setPromotions] =  useState([]);
+
+    useEffect(() => {
+      axios.get('http://localhost:5000/promotions?_embed=comments')
+      .then((response) => {
+        //console.log(response.data);
+        setPromotions(response.data);
+
       }
+      );
+     }, [])
+
+    
      
-    return(
+     return(
         
         <>
-            <PromotionCard promotion={promotion}/>
+          {promotions.map((promotion) =>(
+            <PromotionCard promotion={promotion} key={promotion.id}/>
+           ) )}
+            
         </>
     )
 }
