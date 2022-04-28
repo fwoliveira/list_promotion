5c675c5bda873203b741c0434a59e5d01453d1b0
FRANCISCO WILLIAM DE OLIVEIRA LIMA <francisco.wolima@senacsp.edu.br>
2022-04-27 21:04:37 -0300

    pesquisa na api

--- src/componets/Promotion/Search/Search.jsx
+++ src/componets/Promotion/Search/Search.jsx
@@ -2,33 +2,39 @@ import React, {useEffect, useState} from "react";
 import searchCss from './Search.module.css';
 import axios from 'axios';
 import PromotionCard from '../card/Card';
-import {link} from 'react-router-dom';
 import { Link } from "react-router-dom";
 
 
 const PromotionSearch = () => { 
         const [promotions, setPromotions] =  useState([]);
-        const[ search, setShearch] = useState('');
+        const[ search, setSearch] = useState('');
     
         useEffect(() => {
-          axios.get('http://localhost:5000/promotions?_embed=comments')
+          const params = {};
+          if (search) {
+            params.title_like = search;
+          }
+
+          axios.get('http://localhost:5000/promotions?_embed=comments', { params })
           .then((response) => {
             console.log(response.data);
             setPromotions(response.data);
     
           }
           );
-         }, [])
+         }, [search])
           return (
          <>
-         <header>
+         <header className={searchCss.PromotionSearchHeader}>
            <h1> promo show</h1>
-           <Link to= "">Nova promoção</Link>
+           <Link to= "/create">Nova promoção</Link>
          </header>
          <input type="search"
-         className={searchCss.PromotionSearchIput} 
+         className={searchCss.PromotionSearchInput} 
          placeholder="Busca"
-         value={search}/>
+         value={search}
+         onChange={(ev) => setSearch(ev.target.value)}
+         />
           {promotions.map((promotion) =>(
             <PromotionCard promotion={promotion} key={promotion.id}/>
            ) )}  
